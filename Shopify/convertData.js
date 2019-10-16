27d598f34b463aafd5c637327cdbcc958b816dca
Michael Peterson <WebDev@ctoms.ca>
2019-10-16 10:26:25 -0600

    added build ns item array function

--- Shopify/convertData.js
+++ Shopify/convertData.js
@@ -6,7 +6,23 @@ function convertData(shopifyData){
 }
 //build line item arr for order
 function buildLineItemArr(lineItems){
-
+    let nsItems = [];
+    console.log('shopify line items: ',lineItems[1].price_set);
+    for (let i = 0; i < lineItems.length; i++) {
+        const shopifyItem = lineItems[i];
+        let singleItem = {};
+        //need to match up shopify item codes with netsuite 
+        singleItem.item = shopifyItem.sku;
+        singleItem.quantity = shopifyItem.quantity;
+        //for now hard coded this will = online price
+        //discuss if NS should handle pricing or take pricing from Shopify
+        singleItem.price = 5;
+        //verify if this is individual price or price of quantity
+        singleItem.rate = shopifyItem.price;
+        nsItems.push(singleItem);
+    }
+    console.log('Item array: ',nsItems);
+    return nsItems;
 }
 //build address string for order
 function buildAddressString(addressData){ 
@@ -27,10 +43,13 @@ function buildOrderData(shopifyData){
     orderData.shipaddress = buildAddressString(shopifyData.shipping_address);
     //get shopify shipping code will need to test out with real orders
     orderData.shipmethod = shopifyData.shipping_lines[0].code;
-    orderData.shipmethod = shopifyData.shipping_lines[0].price_set.shop_money.amount;
+    orderData.shippingcost = shopifyData.shipping_lines[0].price_set.shop_money.amount;
     //console.log(shopifyData.shipping_lines);
     orderData.billaddress = buildAddressString(shopifyData.billing_address);
-    
+    //checck this also
+    orderData.otherrefnum = shopifyData.order_number;
+    orderData.items = buildLineItemArr(shopifyData.line_items);
+
     return orderData;
 }
 
