35db6b15f6ebdc3391db5d5404131733ca94d72d
Michael Peterson <WebDev@ctoms.ca>
2019-10-25 12:54:13 -0600

    initial check setup

--- tools/checkKey.js
+++ tools/checkKey.js
@@ -1,34 +1,44 @@
 const {CKEY,DOMAIN,CKEYU,DOMAINU} = require('../config');
 const crypto = require('crypto');
 const getRawBody = require('raw-body');
-const {sanitizeInput} = require('./configTools');
+const {sanitizeInput} = require('./sanitizeInput');
 
 let checkKey = async function(req, res, next){
-
-	let sKey = req.get('X-Shopify-Hmac-SHA256');
-	let domain = req.get('X-Shopify-Shop-Domain');
-	const body = await getRawBody(req)
-	let digest = crypto.createHmac('SHA256', CKEY).update(body).digest('base64');
-	let digestU = crypto.createHmac('SHA256', CKEYU).update(body).digest('base64');
-	if(domain !== DOMAIN && domain !== DOMAINU){
-		return res.status(422).json({
-			code:422,
-			message:"unathorized"
-		});
+	try{
+		let sKey = req.get('X-Shopify-Hmac-SHA256');
+		let domain = req.get('X-Shopify-Shop-Domain');
+		const body = await getRawBody(req)
+		let digest = crypto.createHmac('SHA256', CKEY).update(body).digest('base64');
+		let digestU = crypto.createHmac('SHA256', CKEYU).update(body).digest('base64');
+		if(domain !== DOMAIN && domain !== DOMAINU){
+			return res.status(422).json({
+				code:422,
+				message:"unathorized"
+			});
+		}
+		else if(sKey === digest || sKey === digestU){
+			const order = JSON.parse(body.toString())
+			req.order = order
+			let keys = {
+				shipping_address:{
+					type:'object',
+					keysArray:['address1']
+				}
+			};
+			sanitizeInput(req.order,keys);
+			next();
+		}
+		else{
+			return res.status(422).json({
+				code:422,
+				message:"unathorized"
+			});
+		}
 	}
-	else if(sKey === digest || sKey === digestU){
-		const order = JSON.parse(body.toString())
-		req.order = order
-		sanitizeInput(req.order);
-		next();
-	}
-	else{
-		return res.status(422).json({
-			code:422,
-			message:"unathorized"
-		});
+	catch (e){
+		console.log('error checking key',e);
 	}
 	
-}
+};
 
 module.exports = {checkKey};
--- tools/sanitizeInput.js
+++ tools/sanitizeInput.js
@@ -1,5 +1,24 @@
-let sanitizeInput = function(data) {
-
+function sanitizeObject(objectData,keyArray,replaceRegex){
+    for(let i = 0;i < keyArray.length;i++){
+        let cleanString = objectData[keyArray[i]].replace(replaceRegex,'');
+        objectData[keyArray[i]] = cleanString;
+        console.log(cleanString);
+    }
 }
 
-module.exports = {sanitizeInput}
+let sanitizeInput = function(data,keys) {
+    const replaceRegex = /[\=\<\>\\\/\[\]\{\}]/gi;
+    for(let key in keys){
+        if(!keys[key].type){
+            data[key] = data[key].replace(replaceRegex,'');
+        }
+        else if(keys[key].type === 'object'){
+            sanitizeObject(data[key],keys[key].keysArray,replaceRegex);
+        }
+        else if(keys[key].type === 'array'){
+            
+        }
+    }
+};
+
+module.exports = {sanitizeInput};
