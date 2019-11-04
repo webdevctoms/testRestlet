ba8995aae70ec83943488d169d5a97ebca91e1b1
Michael Peterson <WebDev@ctoms.ca>
2019-11-04 14:35:30 -0700

    initial token creation added

--- routers/getTokenRouter.js
+++ routers/getTokenRouter.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const {CK} = require('../config');
 const {checkRequest} = require('../tools/configTools'); 
 
 router.get('/',checkRequest,(req,res) => {
-    res.send({
-		status:200,
-		message:"Get Token"
-	});
+	try{
+		const token = jwt.sign({
+			data:'token'
+		},CK,{expiresIn:'10m'});
+		res.send({
+			status:200,
+			token
+		});
+	}
+	catch(err){
+		res.send({
+			status:500,
+			err
+		})
+	}
+	
 });
 
 module.exports = {router};
